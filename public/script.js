9c8c3d408560a21b68a0898f2db391dcdc1ab024
Gururaj P Kharvi <gururaj.kharvi@manipalgroup.info>
2022-03-21 13:23:59 +0530

    first commit

--- public/script.js
+++ public/script.js
@@ -7,8 +7,7 @@ const downloadButton = document.querySelector('button#download');
 const recordedVideo = document.querySelector('video#recorded');
 const uploadVideo = document.querySelector('button#upload');
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: process.env.PORT,
+  host: 'https://webrtc-v2.herokuapp.com/',
   config: {'iceServers': [
     { url: 'stun:stun.l.google.com:19302' },
     {
