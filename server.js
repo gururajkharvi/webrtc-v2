8e053d34f91fec210a822bc3657f751de568413e
Gururaj P Kharvi <gururaj.kharvi@manipalgroup.info>
2022-03-21 13:35:02 +0530

    first commit

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ io.on('connection', socket => {
   })
 // server.listen(3000)
 server.listen(process.env.PORT || 3000,'0.0.0.0', () => {
-  console.log("Listening on port 3000...")
+  console.log(`Server is listening on port ${PORT}...`)
 })
